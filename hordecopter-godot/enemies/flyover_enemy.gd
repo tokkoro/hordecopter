b393d8592b9fae6b86256cfaefe63e8fbddd4816
tokko <tomi@valohai.com>
2026-02-07 13:19:25 +0200

    xslow enemies down

--- hordecopter-godot/enemies/flyover_enemy.gd
+++ hordecopter-godot/enemies/flyover_enemy.gd
@@ -14,7 +14,7 @@ class_name FlyoverEnemy
 extends CharacterBody3D
 
 @export var health: float = 4.0
-@export var flyover_speed: float = 18.0
+@export var flyover_speed: float = 4.0
 
 var flyover_enemy_direction: Vector3 = Vector3.FORWARD
 var flyover_enemy_max_health: float = 1.0
--- hordecopter-godot/enemies/medusa_flyer.gd
+++ hordecopter-godot/enemies/medusa_flyer.gd
@@ -17,7 +17,7 @@ class_name MedusaFlyer
 extends CharacterBody3D
 
 @export var health: float = 4.0
-@export var move_speed: float = 16.0
+@export var move_speed: float = 4.0
 @export var wave_amplitude: float = 1.6
 @export var wave_frequency: float = 0.8
 
